edca342898b48ac3aba6299fd55d207f4c9ddfab
Simona Tso <SIMONA@Simonas-MacBook-Pro.local>
2019-05-04 15:17:04 -0400

    idk

--- Rproject/predictive model.R
+++ Rproject/predictive model.R
@@ -1,36 +1,34 @@
 data <- read.csv("/Users/SIMONA/Documents/Datafest/updated.csv", header = TRUE, sep = ',')
-data$illness[data$illness == 'Slightly Off'] <- "Yes"
 data$nutrition[is.na(data$nutrition)] <- "Okay"
-data$illness <- factor(data$illness, levels = c("No", "Yes"))
 data$nutrition <- factor(data$nutrition, levels = c("Poor", "Okay", "Excellent"))
-data$acutechronicratio[data$acutechronicratio >= 1.2] = "High"
-data$acutechronicratio[data$acutechronicratio <= 0.8] = "Low"
-data$acutechronicratio[data$acutechronicratio < 1.2 & data$acutechronicratio > 0.8] = "Medium"
-data$acutechronicratio = factor(data$acutechronicratio, levels = c("Low", "Medium", "High"))
-data$sleephours[is.na(data$sleephours)] <- 6
-for (i in data$sleephours){
+
+for (i in 1:length(data$sleephours)){
   if (data$sleephours[i] >= 0 & data$sleephours[i] < 3){
-    sleephours[i] = "No sleep"
+    data$sleephours[i] = "No sleep"
   }
-  else if (sleephours[i] >= 3 & sleephours[i] < 6){
-    sleephours[i] = "Deprived"
+  else if (data$sleephours[i] >= 3 & data$sleephours[i] < 6){
+    data$sleephours[i] = "Deprived"
   }
-  else if (sleephours[i] >= 6 & sleephours[i] < 9){
-    sleephours[i] = "Adequate"
+  else if (data$sleephours[i] >= 6 & data$sleephours[i] < 9){
+    data$sleephours[i] = "Adequate"
   }
-  else if (sleephours[i] >= 9 & sleephours[i] < 14.5){
-    sleephours[i] = "Plenty"
+  else if (data$sleephours[i] >= 9 & data$sleephours[i] < 14.5){
+    data$sleephours[i] = "Plenty"
   }
-  else {
-    sleephours[i] = "Coma"
-  }
+  else (data$sleephours[i] = "Coma")
 }
-data$sleephours[data$sleephours >= 0 & data$sleephours <= 3] = "No sleep"
-data$sleephours[data$sleephours > 3 & data$sleephours <= 6] = "Deprived"
-data$sleephours[data$sleephours > 6 & data$sleephours <= 9] = "Adequate"
-data$sleephours[data$sleephours > 9 & data$sleephours <= 14.5] = "Plenty"
-data$sleephours[data$sleephours > 14.5 & data$sleephours <= 22.5] = "Coma"
+
 data$sleephours <- factor(data$sleephours, levels = c("No sleep", "Deprived", "Adequate", "Plenty", "Coma"))
-# data <- data %>% 
-#   mutate(nutrition = relevel(nutrition, ref = "Okay"))
-str(data)
+set.seed(1)
+sample_size <- floor(0.8*nrow(data))
+train_ind <- sample(seq_len(nrow(data)), size = sample_size, replace = FALSE)
+train <- data[train_ind,]
+test <- data[-train_ind,]
+model <- lm(fatigue~ irritability + sleephours + sleepquality + pain + nutrition, data = data)
+summary(model)
+anova(model)
+plot(predict(model), model$fatigue[-train_ind,], xlab="Predicted", ylab="actual")
+
+data$prediction <- predict(model)
+
+write.csv(data,"/Users/SIMONA/Documents/Datafest/model.csv")
