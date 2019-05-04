b940787ccea5d8abb1e3a08a3bde0ffa6ca6b23a
Simona Tso <simona.tso@mail.utoronto.ca>
2019-05-04 17:32:16 -0400

    final stuff2

--- Rproject/predictive model.R
+++ Rproject/predictive model.R
@@ -1,34 +1,16 @@
 data <- read.csv("/Users/SIMONA/Documents/Datafest/updated.csv", header = TRUE, sep = ',')
 data$nutrition[is.na(data$nutrition)] <- "Okay"
-data$nutrition <- factor(data$nutrition, levels = c("Poor", "Okay", "Excellent"))
-
-for (i in 1:length(data$sleephours)){
-  if (data$sleephours[i] >= 0 & data$sleephours[i] < 3){
-    data$sleephours[i] = "No sleep"
-  }
-  else if (data$sleephours[i] >= 3 & data$sleephours[i] < 6){
-    data$sleephours[i] = "Deprived"
-  }
-  else if (data$sleephours[i] >= 6 & data$sleephours[i] < 9){
-    data$sleephours[i] = "Adequate"
-  }
-  else if (data$sleephours[i] >= 9 & data$sleephours[i] < 14.5){
-    data$sleephours[i] = "Plenty"
-  }
-  else (data$sleephours[i] = "Coma")
-}
-
-data$sleephours <- factor(data$sleephours, levels = c("No sleep", "Deprived", "Adequate", "Plenty", "Coma"))
-set.seed(1)
-sample_size <- floor(0.8*nrow(data))
-train_ind <- sample(seq_len(nrow(data)), size = sample_size, replace = FALSE)
-train <- data[train_ind,]
-test <- data[-train_ind,]
+data$nutrition <- factor(data$nutrition, levels = c("Okay", "Poor", "Excellent"))
+data$sleephours <- cut(data$sleephours, c(0, 3, 6, 9, 14.5, Inf))
+levels(data$sleephours) <- c("No Sleep", "Deprived", "Adequate", "Plenty", "Coma")
+data$sleephours <- relevel(data$sleephours, ref = "Adequate")
+levels(data$sleephours)
+str(data)
 model <- lm(fatigue~ irritability + sleephours + sleepquality + pain + nutrition, data = data)
 summary(model)
 anova(model)
-plot(predict(model), model$fatigue[-train_ind,], xlab="Predicted", ylab="actual")
-
 data$prediction <- predict(model)
-
+data$residuals <- model$residuals
+par(mfrow = c(2,2))
+plot(model)
 write.csv(data,"/Users/SIMONA/Documents/Datafest/model.csv")
